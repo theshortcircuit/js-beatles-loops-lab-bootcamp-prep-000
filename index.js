02a8a72763d080b57c4906aa92a752669e8aeae6
Cody Short <nccgs117@gmail.com>
2018-01-25 21:45:49 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,8 @@
+function theBeatlesPlay(musicians,instruments){
+  var array = []
+  for(let i=0,i<musicians.length(),i++){
+    var string = `musicians[i] plays instruments[i]`
+    array[i]=string
+  }
+  retun array
+}
